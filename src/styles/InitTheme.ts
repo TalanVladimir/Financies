cb16690cac1887ae4cbb917fe726cf29fdba5b11
Vladimir Talan <vovkus@gmail.com>
2022-11-03 00:13:38 +0200

    fork: add Themes

---
+++ src/styles/InitTheme.ts
@@ -0,0 +1,16 @@
+import {ThemeType} from '../types/theme';
+
+export const InitTheme: ThemeType = {
+  isDark: false,
+  colors: {
+    primaryDark: '',
+    primaryLight: '',
+    primary: '',
+    text: '',
+    accent: '',
+    primaryText: '',
+    secondaryText: '',
+    divider: '',
+    white: '',
+  },
+};
---
+++ src/styles/LightTheme.ts
@@ -0,0 +1,16 @@
+import {ThemeType} from '../types/theme';
+
+export const LightTheme: ThemeType = {
+  isDark: false,
+  colors: {
+    primaryDark: '#FF8C00',
+    primaryLight: '#FFCB8D',
+    primary: '#FFAC46',
+    text: '#FFFFFF',
+    accent: '#FF5722',
+    primaryText: '#774612',
+    secondaryText: '#FF7300',
+    divider: '#D3D3D3',
+    white: '#FFFFFF',
+  },
+};
