03386a92461460c603f1e04b7b956f600a005e06
Vladimir Talan <vovkus@gmail.com>
2022-11-05 20:11:44 +0200

    fork: add Buy

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {FinanciesScreen} from '../../screens/FinanciesScreen/FinanciesScreen';
 import {ProfileScreen} from '../../screens/ProfileScreen/ProfileScreen';
 import {LoginScreen} from '../../screens/LoginScreen/LoginScreen';
 import {Panel} from '../Panel/Panel';
 import {RegisterScreen} from '../../screens/RegisterScreen/RegisterScreen';
 import {useAuthState} from 'react-firebase-hooks/auth';
 import {auth} from '../../firebase/firebase';
+import {BuyScreen} from '../../screens/BuyScreen/BuyScreen';
+import {FinanciesScreen} from '../../screens/FinanciesScreen/FinanciesScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,8 +17,9 @@ export const Navigation: React.FC = (): JSX.Element => {
 
   const ShowScreens = user ? (
     <>
-      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Buy" component={BuyScreen} />
       <Stack.Screen name="Financies" component={FinanciesScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
     </>
   ) : (
     <>
@@ -43,6 +45,7 @@ export const Navigation: React.FC = (): JSX.Element => {
       <Stack.Navigator
         screenOptions={{
           headerShown: true,
+          headerBackVisible: false,
         }}>
         {ShowScreens}
       </Stack.Navigator>
