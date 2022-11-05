dd2b36575362af230116daf0129115e423bdcff3
Vladimir Talan <vovkus@gmail.com>
2022-11-05 21:01:46 +0200

    fork: add Navigation hide on input active

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {ProfileScreen} from '../../screens/ProfileScreen/ProfileScreen';
@@ -9,11 +9,37 @@ import {useAuthState} from 'react-firebase-hooks/auth';
 import {auth} from '../../firebase/firebase';
 import {BuyScreen} from '../../screens/BuyScreen/BuyScreen';
 import {FinanciesScreen} from '../../screens/FinanciesScreen/FinanciesScreen';
+import {Keyboard} from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
+const useHideWhenKeyboardOpen = () => {
+  const [isKeyboadVisible, setIsKeyboadVisible] = useState(false);
+
+  const _keyboardDidShow = useCallback(() => {
+    setIsKeyboadVisible(true);
+  }, []);
+
+  const _keyboardDidHide = useCallback(() => {
+    setIsKeyboadVisible(false);
+  }, []);
+
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', _keyboardDidShow);
+    Keyboard.addListener('keyboardDidHide', _keyboardDidHide);
+
+    return () => {
+      Keyboard.addListener('keyboardDidShow', _keyboardDidShow);
+      Keyboard.addListener('keyboardDidHide', _keyboardDidHide);
+    };
+  }, [_keyboardDidHide, _keyboardDidShow]);
+
+  return isKeyboadVisible;
+};
+
 export const Navigation: React.FC = (): JSX.Element => {
   const [user] = useAuthState(auth);
+  const isKeyboardShown = useHideWhenKeyboardOpen();
 
   const ShowScreens = user ? (
     <>
@@ -49,7 +75,7 @@ export const Navigation: React.FC = (): JSX.Element => {
         }}>
         {ShowScreens}
       </Stack.Navigator>
-      {user && <Panel />}
+      {user && !isKeyboardShown && <Panel />}
     </NavigationContainer>
   );
 };
