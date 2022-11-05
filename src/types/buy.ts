d9df9324c8ba1d0047212b9be68b0108a78d88f7
Vladimir Talan <vovkus@gmail.com>
2022-11-05 20:09:23 +0200

    fork: add Buy Types

---
+++ src/types/buy.ts
@@ -0,0 +1,14 @@
+export interface BuyItemType {
+  id: number;
+  category: string;
+  product: string;
+  multiply: number;
+  price: number;
+  email: string;
+}
+
+export interface BuyProductType {
+  id: number;
+  category: string;
+  product: string;
+}
